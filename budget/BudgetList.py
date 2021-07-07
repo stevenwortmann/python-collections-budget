15198f4c326488b417e6a96478899fc6896e3b80
Steve <60276652+stevenwortmann@users.noreply.github.com>
2021-07-07 15:37:28 -0400

    Create the BudgetList class to Display Budget Overages

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -29,6 +29,7 @@ def main():
     for expense in expenses.list:
         myBudgetList.append(expense.amount)
 
+    print(myBudgetList)
     print('The count of all expenses: ' + str(len(myBudgetList)))
 
 if __name__ == "__main__":
